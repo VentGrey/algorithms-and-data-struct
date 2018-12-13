c96e0706e8a4caf48647d79863d647d65c9ca900
VentGrey <omarpurataf@gmail.com>
2018-12-13 17:40:03 -0600

    Finished first file [Not tested]

--- C-C++/1/intro.cpp
+++ C-C++/1/intro.cpp
@@ -3,10 +3,23 @@
 
 using namespace std;
 
-ifstream f("data.in");
-ofstream g("data.out");
+//Input File Stream
+ifstream f("data.in"); // f es el identificador de los datos de entrada.
+
+//Output File Stream
+ofstream g("data.out"); // g es el identificador de los datos de salida.
 
 int main() { 
-        cout << "Hello World";
+        int x, y, sum; // Ejemplo burdo con 3 variables
+
+        f >> x >> y; 
+        // En el archivo f leer el primer valor y asignarlo a X, después el
+        //segundo valor y asignarlo a y.
+
+        sum = x + y; // Sumar los 2 valores leidos.
+
+        g << sum; // Escribir en el archivo el valor de suma
+
+
         return 0;
 }
