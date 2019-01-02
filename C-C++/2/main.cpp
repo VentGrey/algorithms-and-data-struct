7efa5d5577c1c1b502a1291f4e74f9a51e71bb4e
VentGrey <omarpurataf@gmail.com>
2019-01-01 18:58:58 -0600

    Added second practice

--- C-C++/2/main.cpp
+++ C-C++/2/main.cpp
@@ -1,8 +1,26 @@
-#include <iostream>
 #include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
-int main() {
+ifstream f("data.in");
+ofstream g("data.out");
+
+int main_arr[100], n;
+
+int main()
+{
+        f >> n;
+        for (int i = 0; i <= n; i++) {
+                int x;
+                f >> x;
+                ++main_arr[x];
+        }
+        if (main_arr[4] > 0) {
+                g << "Aparece";
+        } else {
+                g << "No aparece";
+        }
+
         return 0;
 }
