163b4f0c937fe7ca8f350d0574b98e5db9b42833
VentGrey <omarpurataf@gmail.com>
2019-01-31 21:53:09 -0600

    Added fractal generator in C

---
+++ C-C++/C-Fractal/main.c
@@ -0,0 +1,30 @@
+/*
+ * ============================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  Un programa en C que genera un Fractal de Mandelbrot.
+ *
+ *        Sources: La información y el código creado fueron posibles gracias a
+ *                 las siguientes fuentes de información:
+ *
+ *                Tutorial de WikiHow:
+ *       - https://es.wikihow.com/dibujar-el-conjunto-de-Mandelbrot-manualmente
+ *
+ *                 Tutorial con Java
+ *       - https://underc0de.org/foro/java/conjunto-de-mandelbrot-(fractal)/
+ *
+ *
+ *
+ *        Version:  1.0
+ *        Created:  31/01/19 14:23:32
+ *       Revision:  1
+ *       Compiler:  gcc-6+ or clang-6+
+ *
+ *         Author:  Omar Jair Purata Funes (VentGrey), omarpurataf@gmail.com
+ *   Organization:  Universidad de Guanajuato
+ *
+ * ============================================================================
+ */
+
+
