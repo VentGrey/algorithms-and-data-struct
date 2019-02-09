bda29595203d26f5bd52a20fc2aa7a8b99a4beba
VentGrey <omarpurataf@gmail.com>
2019-02-09 15:17:29 -0600

    Added new constructors

--- C-C++/C-Fractal/main.cpp
+++ C-C++/C-Fractal/main.cpp
@@ -12,26 +12,47 @@
 
 /* Clase principal para el manejo de imágenes
  * "p" delimiará que atributos son privados porque en C++ no se puede*/
-class Imagen {
+class PGM {
         int plon;
         int palt;
-        std::vector<uint32_t> pdatos; //Vector de enteros sin signo (32 bits)
+        std::vector<uint32_t> ppuntos; //Vector de enteros sin signo (32 bits)
 
         public:
+
+
+                // Constructor de longitud
+                int lon() {
+                        return plon;
+                }
+
+                // Constructor de altura
+                int alt() {
+                        return palt;
+                }
+
+                // Constructor de pixeles dentro del PGM
+                int pixel() {
+                        return palt * plon;
+                }
+
                 // Constructor por defecto de la clase
                 // (Generará una imagen de tamaño 0,0)
-                Imagen():
+                PGM():
                         plon(0),
                         palt(0)
                 {}
 
                 // Constructor con parámetros (los valores comienzan por 0)
-                Imagen(unsigned int lon, unsigned int alt)
+                PGM(int lon, int alt)
                 : plon(lon)
                 , palt(alt)
-                , pdatos(lon * alt, uint32_t())
+                , ppuntos(lon * alt, uint32_t())
                 {}
 
+                // Si deseamos rellenar el fractal necesitamos escalar los
+                // pixeles anteriormente definidos, afortunadamente
+                // https://www.geeksforgeeks.org/fractals-in-cc/ existe
+                // para medio-fusilarse eso
 };
 
 
