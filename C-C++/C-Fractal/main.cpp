3a819e6d51b56f033e790e562404ab252f650155
VentGrey <omarpurataf@gmail.com>
2019-02-09 21:50:42 -0600

    FINALLY GENERATES AN IMAGE

--- C-C++/C-Fractal/main.cpp
+++ C-C++/C-Fractal/main.cpp
@@ -13,41 +13,27 @@ int mand(complex<double> z0, int lim);
 //--- MAIN
 int main(int argc, char const *argv[])
 {
-
-        if (argc > 3) {
-                cout << "Faltan operandos en el programa" << endl;
-                cout << "Pruebe 'fract -h' para mas información" << endl;
-                return 1;
-        } else if (std::string(argv[0]) == "-h") {
-                cout << "Modo de empleo: fract [X0] [Y0] [SIZE]\
-                        [NxN dimensions]" << endl;
-                cout << "Estados de salida:" << endl;
-                cout << "0 -- Todo fue bien" << endl;
-                cout << "1 -- Problema menor" << endl;
-                cout << "2 -- Problema mayor" << endl;
-                return 0;
-        }
-
         // El fractal estará centrado en...
-        double xC = atof(argv[0]);
-        double yC = atof(argv[1]);
-        double tam = atof(argv[2]);
-        int n = atoi(argv[3]); // Dimensiones de la imágen (De nxn)
+        double xC = 100;
+        double yC = 100;
+        double tam = 10;
+        int n = 255; // Dimensiones de la imágen (De nxn)
         int max = 255; // Número de iteraciones
 
         ofstream mandelbrot("fractal.pgm");
         // Escribir la cabecera del archivo pgm
-        mandelbrot << "P2" << endl << n << "" << n << endl << tam << endl;
+        mandelbrot << "P2" << endl << n << " " << n << endl << tam << endl;
 
         for (int i = 0; i < n; i++) {
                 for (int j = 0; j < n; j++) {
-                        double x0 = xC - tam/2 + tam * i/n;
-                        double y0 = yC - tam/2  + tam * j/n;
-
+                        int x0 = xC - tam/2 + tam * i/n;
+                        int y0 = yC - tam/2  + tam * j/n;
+                        mandelbrot << x0 << " " << y0;
                 }
+                mandelbrot << endl;
         }
+        mandelbrot.close();
 
-        mandelbrot << 
         return 0;
 }
 
