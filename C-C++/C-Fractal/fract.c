9f55706aece9b37697e553fa1e7975d421ca94b7
VentGrey <omarpurataf@gmail.com>
2019-02-11 17:42:54 -0600

    Added a shitload of new variables

--- C-C++/C-Fractal/fract.c
+++ C-C++/C-Fractal/fract.c
@@ -10,8 +10,35 @@ complex double mandelbrot(int max, complex double z0, complex double c);
 //--- MAIN
 int main(int argc, char const *argv[])
 {
-        int n = 128;
-        int matrix [n][n];
+        const int n = 128;
+        const int m = 128;
+        int iter = 10000;
+        int matrix [m][n];
+        int pcalc = 1000;
+
+        // Para discretizar la función
+        const double sup_real = 0.5;
+        const double inf_real = -1.5;
+        const double sup_imag = 1.0;
+        const double inf_imag = -1.0;
+
+        double puntoX = ((sup_real - inf_real) / pcalc);
+        double puntoY = ((sup_imag - inf_imag) / pcalc);
+
+        if (argc <= 0) {
+                printf("No se proporcionaron argumentos, saliendo...");
+                return 0;
+        }
+
+        for(int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                }
+        }
+
+
+
+
+
         return 0;
 }
 
