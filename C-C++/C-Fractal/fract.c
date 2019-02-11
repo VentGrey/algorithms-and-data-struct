a43464a2f394a1b28bd4a662ec4fc1c2d228d6e9
VentGrey <omarpurataf@gmail.com>
2019-02-11 14:40:43 -0600

    Migrated to C, again

--- C-C++/C-Fractal/fract.c
+++ C-C++/C-Fractal/fract.c
@@ -1,8 +1,10 @@
 //--- Preprocesador
 #include <cstdlib> // Para conversión de cadena a doble
-#include <complex.h> // Biblioteca para números complejos
+#include <complex> // Biblioteca para números complejos
 #include <iostream>
 #include <fstream>
+
+int n = 128;
 
 //--- Namespaces
 using namespace std;
@@ -15,11 +17,8 @@ int main(int argc, char const *argv[])
 {
         // El fractal estará centrado en...
         double tam = 255;
-        int n = 128; // Dimensiones de la imagen (n)
-        int m = 128; // Dimensiones de la imagen (m)
         int max = 255; // Número de iteraciones
 
-        int matrix [m][n];
         ofstream mandelbrot("fractal.pgm");
         // Escribir la cabecera del archivo pgm
         mandelbrot << "P2\n" << n << " " << n << endl << tam << endl;
@@ -30,17 +29,10 @@ int main(int argc, char const *argv[])
 
 //-- Retornar el número de iteraciones para revisar si c = a + ib pertenece al
 //-- conjunto de mandelbrot
-int mand(complex<double> z0, int max)
-{
-        complex<double> Z = z0;
-
-        for (int i = 0; i < max; i++) {
-                if (abs(Z) > 2.0) {
-                        return i;
-                } else {
-                        Z = Z * Z + z0;
-                }
-
+int mandelbrot(int n, int z, int c) {
+        if (n > 0) {
+                return mandelbrot(n-1, pow(z, 2) + c, c);
+        } else {
+                return z;
         }
-        return max;
 }
