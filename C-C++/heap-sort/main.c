e2474fbe7f269976f68307297f6b4b2f2bb956a9
VentGrey <omarpurataf@gmail.com>
2019-04-01 18:39:08 -0600

    Heap 'constructor'

--- C-C++/heap-sort/main.c
+++ C-C++/heap-sort/main.c
@@ -6,5 +6,17 @@ void down_adjust(int [], int);
 
 int main(int argc, char *argv[]) {
     int heap[30], n, last, temp;
+
+    printf("Ingrese el número de elementos: ");
+    scanf("%d", &n);
+
+    printf("Ingrese los elementos: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &heap[i]);
+    }
+
+    heap[0] = n;
+    create(heap);
+
     return 0;
 }
