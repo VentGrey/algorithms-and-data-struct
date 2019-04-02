05afeba38010b9cbc247ed3b71e6206f28265b35
VentGrey <omarpurataf@gmail.com>
2019-04-01 18:27:26 -0600

    finished circle area

--- Rust/practice/src/circle/mod.rs
+++ Rust/practice/src/circle/mod.rs
@@ -1,14 +1,28 @@
 /* Escriba un programa tal que dado el radio de un cı́rculo, calcule e imprima
 el área y la circunferencia del mismo.*/
 
+use std::f64::consts::PI;
 use std::io;
 
 pub fn circle() {
     println!("Ingrese el radio de su círculo");
     let mut scanner = String::new();
 
-    let rad: f64 = 0.0;
     io::stdin()
         .read_line(&mut scanner)
         .expect("Ocurrió un error al leer desde el teclado");
+
+    let num1 = scanner.trim();
+
+    match num1.parse::<f64>() {
+        Ok(num1) => {
+            println!("El radio se registró correctamente! {}", num1);
+            let area: f64 = num1 * num1 * PI;
+            println!("El área del círculo es: {}", area);
+        }
+        Err(e) => {
+            eprintln!("Esto no es un número {}", num1);
+            eprintln!("Error: {}", e.to_string());
+        }
+    }
 }
