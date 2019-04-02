9034ef4162a5ddc8559271c48063d506a017e2f4
VentGrey <omarpurataf@gmail.com>
2019-04-01 23:31:14 -0600

    Added one max header

---
+++ C-C++/UMDA-ONEMAX/onemax.h
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+
+
+#ifndef __ONEMAX_H_
+#define __ONEMAX_H_
+
+
+
+
+#endif // __ONEMAX_H_
+
+// -- Prototipos de cabecera
+
+static float max();
+static int compare();
+
+
+
+// Funciones de cabecera
+
+// Esta función muestra la diferencia de
+static int compare(void* p1, void* p2) {
+    int* arr = (int *)p1;
+    int* arr1 = (int *)p2;
+
+    int diff = arr[0] - arr1[0];
+}
+
+static float max() {
+    return (float)rand() / (float)RAND_MAX;
+}
