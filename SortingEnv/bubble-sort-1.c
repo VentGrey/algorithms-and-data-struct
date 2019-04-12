0a0c198c83b2a88051ddb1badf6668317f3b9df2
VentGrey <omarpurataf@gmail.com>
2019-04-12 18:11:03 -0500

    Minor changes

--- SortingEnv/bubble-sort-1.c
+++ SortingEnv/bubble-sort-1.c
@@ -8,8 +8,23 @@ void swap(int *a, int *b);
 
 
 //-- Función main
+int main(int argc, char *argv[])
+{
 
+    return 0;
+}
 
 
 
 //-- Funciones externas
+
+
+
+
+// Cambio de variables con punteros
+void swap(int *a, int *b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
