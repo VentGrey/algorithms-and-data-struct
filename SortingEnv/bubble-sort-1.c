e4c1671dc274d256cacf048f2699f76806d06bb2
VentGrey <omarpurataf@gmail.com>
2019-04-12 18:11:03 -0500

    Minor changes

--- SortingEnv/bubble-sort-1.c
+++ SortingEnv/bubble-sort-1.c
@@ -8,8 +8,18 @@ void swap(int *a, int *b);
 
 
 //-- Función main
+int main(int argc, char *argv[])
+{
 
+    return 0;
+}
 
 
 
 //-- Funciones externas
+void swap(int *a, int *b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
