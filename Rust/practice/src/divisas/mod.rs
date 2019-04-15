bf794329ac98f20d7a48019ed3dbc614d15e9013
VentGrey <omarpurataf@gmail.com>
2019-04-14 22:17:53 -0500

    Se terminó el módulo de divisas

--- Rust/practice/src/divisas/mod.rs
+++ Rust/practice/src/divisas/mod.rs
@@ -1,19 +1,32 @@
 /*
 * En una Casa de Cambio necesitan construir un programa tal que dado como
-* dato una cantidad expresada en dólares, convierta esa cantidad a pesos. 
+* dato una cantidad expresada en dólares, convierta esa cantidad a pesos.
 * Elabore un programa que permita resolver el problema.
 */
 
 use std::io;
 
-fn main() {
+pub fn divisas() {
     println!("Ingrese la cantidad de dólares que posee: ");
-    let mut Scanner = String::new();
-    let mut dlls: f64 = 0.0;
+    let mut scanner = String::new();
 
-    io::stdin().read_line(&mut Scanner)
-    .expect("Error al leer desde teclado")
+    io::stdin()
+        .read_line(&mut scanner)
+        .expect("Error al leer desde teclado");
 
-    const pesos:f64 = 18.75;
+    const VALORPESO: f64 = 18.75;
 
+    let dlls = scanner.trim();
+    let cambio: f64;
+
+    match dlls.parse::<f64>() {
+        Ok(dlls) => {
+            cambio = dlls * VALORPESO;
+        }
+        Err(_e) => {
+            eprintln!("Esto no es un número válido {}", _e);
+            panic!();
+        }
+    }
+    println!("Usted posee {} pesos mexicanos", cambio);
 }
