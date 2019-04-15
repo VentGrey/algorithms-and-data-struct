078a506034b2d55640c9ef99bcc4368d6608fcc4
VentGrey <omarpurataf@gmail.com>
2019-04-15 01:18:19 -0500

    Removed tiny java lines

--- Java/EjemploSencillo/src/ejemplosencillo/base/IFuncionObjetivo.java
+++ Java/EjemploSencillo/src/ejemplosencillo/base/IFuncionObjetivo.java
@@ -2,10 +2,6 @@ package ejemplosencillo.base;
 
 import java.util.LinkedList;
 
-/**
- *
- * @author Marco Sotelo
- */
 public interface IFuncionObjetivo{
     public Integer evaluar(LinkedList<Byte> values);
 }
