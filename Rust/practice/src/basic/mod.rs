bd54b060332ea79ea71d557d245b72c96d1be8e5
VentGrey <omarpurataf@gmail.com>
2019-04-14 21:01:52 -0500

    added missing operations and errors

--- Rust/practice/src/basic/mod.rs
+++ Rust/practice/src/basic/mod.rs
@@ -1,50 +1,61 @@
 /*Escriba un programa que dado como datos dos números reales, calcule la
 suma, resta y multiplicación de dichos números.*/
 
+use std::error::Error;
 use std::io;
 
 pub fn basic_op() {
-    println!("Please input your first number");
+    println!("Ingrese su primer número");
     let mut scanner = String::new();
 
     let mut sum: f64 = 0.0;
+    let mut sub: f64 = 0.0;
+    let mut mult: f64 = 0.0;
+    let mut div: f64 = 0.0;
 
     io::stdin()
         .read_line(&mut scanner)
-        .expect("Something went wrong when reading user input");
+        .expect("Ocurrió un error al leer la entrada");
 
     let num_1 = scanner.trim();
 
     match num_1.parse::<f64>() {
         Ok(num_1) => {
-            println!("Number 1 registered correctly! {}", num_1);
-            sum += num_1;
+            sum = num_1;
+            sub = num_1;
+            mult = num_1;
+            div = num_1;
         }
         Err(e) => {
-            eprintln!("This is not a number {}", num_1);
-            eprintln!("Error: {}", e.to_string());
+            eprintln!("Esto no es un número: {}", num_1);
+            eprintln!("Error: {}", e.description());
         }
     }
 
-    println!("Input yout second number");
+    println!("Ingrese su segundo número");
     let mut scanner = String::new();
     io::stdin()
         .read_line(&mut scanner)
-        .expect("Something went wrong when reading user input");
+        .expect("Ocurrió un error al leer la entrada");
 
     let num_2 = scanner.trim();
 
     match num_2.parse::<f64>() {
         Ok(num_2) => {
-            println!("Number 2 registered correctly! {}", num_2);
             sum += num_2;
+            sub -= num_2;
+            mult *= num_2;
+            div /= num_2;
         }
         Err(e) => {
             eprintln!("This is not a number {}", num_2);
-            eprintln!("Error: {}", e);
+            eprintln!("Error: {}", e.description());
             panic!("Cannot recover from error, exiting");
         }
     }
 
-    println!("The sum of {} and {} is: {}", num_1, num_2, sum);
+    println!("The sum of {} + {} is: {}", num_1, num_2, sum);
+    println!("The diff of {} - {} is: {}", num_1, num_2, sub);
+    println!("The product of {} * {} is: {}", num_1, num_2, mult);
+    println!("The division of {} / {} is: {}", num_1, num_2, div);
 }
