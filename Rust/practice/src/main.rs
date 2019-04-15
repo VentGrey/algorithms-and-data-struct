fd5c8a6a083aa7b061235b6f6293821fdf04541b
VentGrey <omarpurataf@gmail.com>
2019-04-14 22:18:05 -0500

    se añadió el módulo al menú principal

--- Rust/practice/src/main.rs
+++ Rust/practice/src/main.rs
@@ -1,5 +1,6 @@
 mod basic;
 mod circle;
+mod divisas;
 
 use std::io;
 
@@ -8,6 +9,7 @@ fn main() {
     println!("Seleccione el código a ejecutar:");
     println!("1- Operaciones básicas");
     println!("2- Área y perímetro del círculo");
+    println!("3- Cambio de divisas");
 
     let mut entrada = String::new();
     io::stdin().read_line(&mut entrada).unwrap();
@@ -16,6 +18,7 @@ fn main() {
     match num {
         1 => basic::basic_op(),
         2 => circle::circle(),
+        3 => divisas::divisas(),
         _ => panic!("Error de entrada D:"),
     }
 }
