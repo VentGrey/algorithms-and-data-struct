4e834bf980d407bffc3414ebe8b647bb38b00614
VentGrey <omarpurataf@gmail.com>
2019-04-14 21:58:04 -0500

    added main func

--- Rust/practice/src/divisas/mod.rs
+++ Rust/practice/src/divisas/mod.rs
@@ -1,7 +1,19 @@
-/////////////////////////////////////////////////////////////////////////////////////
-// En una Casa de Cambio necesitan construir un programa tal que dado como         //
-// dato una cantidad expresada en dólares, convierta esa cantidad a pesos. Elabore //
-// un programa que permita resolver el problema.                                   //
-/////////////////////////////////////////////////////////////////////////////////////
+/*
+* En una Casa de Cambio necesitan construir un programa tal que dado como
+* dato una cantidad expresada en dólares, convierta esa cantidad a pesos. 
+* Elabore un programa que permita resolver el problema.
+*/
 
 use std::io;
+
+fn main() {
+    println!("Ingrese la cantidad de dólares que posee: ");
+    let mut Scanner = String::new();
+    let mut dlls: f64 = 0.0;
+
+    io::stdin().read_line(&mut Scanner)
+    .expect("Error al leer desde teclado")
+
+    const pesos:f64 = 18.75;
+
+}
