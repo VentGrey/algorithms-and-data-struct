fedbd1c05b8b9800ba0f15e0075d6475b629cad9
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-05-04 02:09:29 -0500

    Finished example

--- Rust/heaps-and-stacks/src/main.rs
+++ Rust/heaps-and-stacks/src/main.rs
@@ -1,3 +1,12 @@
+use std::mem;
+
+struct Struct {
+    a: u8,
+    b: u8,
+    c: u8
+}
+
 fn main() {
-    println!("Hello, world!");
+    assert_eq!(mem::size_of::<Struct>(), 3 * mem::size_of::<u8>());
+    assert_eq!(mem::size_of::<[Struct; 2]>(), 3 * mem::size_of::<u8>() * 2);
 }
