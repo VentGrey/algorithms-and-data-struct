370bd0c1740b213aab8da4bbba3a6544da54c7c4
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-05-09 00:58:42 -0500

    New impl function

--- Rust/listas/src/first.rs
+++ Rust/listas/src/first.rs
@@ -14,8 +14,28 @@ struct Node {
     nect: Link,
 }
 
+/*
+ fn foo(self, arg2: Type2) -> ReturnType {
+    // body
+}
+
+donde:
+
+self - valor
+&mut self - referencia mutable
+&self - referencia compartida
+
+ */
+
 impl List {
-    pub fn new() -> Self {
+    pub fn new() -> Self { // Con Self nos ahorramos el volver a escribir tipos
         List { head: Link::Empty }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Node {
+            elem: elem,
+            next: self.head,
+        }
     }
 }
