7318529b911900ff52643ba57a52d8802055de59
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-05-09 00:46:46 -0500

    Added public structure

--- Rust/listas/src/first.rs
+++ Rust/listas/src/first.rs
@@ -1,12 +1,15 @@
 // Solo vamos a permitir los enteros de 32 bits
 
+pub struct List {
+    head: Link,
+}
+
+enum Link {
+    Empty,
+    More(Box<Node>),
+}
 
 struct Node {
     elem: i32,
-    next: List,
-}
-
-pub enum List { //Con "pub" podemos usar los enums fuera del archivo
-    Empty,
-    More(Box<Node>),
+    nect: Link,
 }
