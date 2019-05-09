ddebfbc183d6e833b31a548ee2972b82ca6a478c
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-05-09 01:16:23 -0500

    Finished pop function

--- Rust/listas/src/first.rs
+++ Rust/listas/src/first.rs
@@ -44,6 +44,12 @@ impl List {
     }
 
     pub fn pop(&mut self) -> Option<i32> {
-
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None,
+            Link::More(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
     }
 }
