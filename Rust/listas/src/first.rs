3aef4d723ca074b4a36546888974ce1b8cf94c6d
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-05-09 00:44:09 -0500

    Changed list structure

--- Rust/listas/src/first.rs
+++ Rust/listas/src/first.rs
@@ -1,7 +1,12 @@
 // Solo vamos a permitir los enteros de 32 bits
 
-pub enum List { //Con "pub" podemos usar los enums fuera del archivo
-    Empty,
-    Elem(i32, Box<List>),
+
+struct Node {
+    elem: i32,
+    next: List,
 }
 
+pub enum List { //Con "pub" podemos usar los enums fuera del archivo
+    Empty,
+    More(Box<Node>),
+}
