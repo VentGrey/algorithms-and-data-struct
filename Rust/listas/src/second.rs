a307aa03b84133ac14219d4377a08f74804ec396
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-05-14 17:25:06 -0500

    Add peek func

--- Rust/listas/src/second.rs
+++ Rust/listas/src/second.rs
@@ -31,6 +31,12 @@ impl<T> List<T> {
             node.elem
         })
     }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| {
+            &node.elem
+        })
+    }
 }
 
 impl<T> Drop for List<T> {
