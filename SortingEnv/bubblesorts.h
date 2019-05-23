14d612b0631f640eb97ecbc343bb06d474d9851f
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-05-22 22:33:16 -0500

    Fuck it, pointers galore

--- SortingEnv/bubblesorts.h
+++ SortingEnv/bubblesorts.h
@@ -9,7 +9,7 @@
 void bubble(int arr[]);
 
 
-void bubble(int arr[]) {
+void bubble(int *arr) {
         int n = sizeof(arr);
         for (int i = 0; i < n - 1; i++) {
                 for (int j = 0; j < n - i - 1; j++) {
