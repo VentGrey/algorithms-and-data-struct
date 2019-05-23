a41995ca2da6294348ca2e2c649295f5f148bad9
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-05-23 14:38:41 -0500

    improved bubblesort

--- SortingEnv/bubblesorts.h
+++ SortingEnv/bubblesorts.h
@@ -1,8 +1,8 @@
 #ifndef __BUBBLESORTS_H_
 #define __BUBBLESORTS_H_
 
-#include < stdio.h >
-#include < stdbool.h >
+#include <stdio.h>
+#include <stdbool.h>
 
 
 // -- prototyped
@@ -24,9 +24,23 @@ void bubble(int *arr)
         }
 }
 
-void bubble_imp()
+void bubble_imp(int *arr)
 {
+        int tmp = 0;
 
+        for (int i = 0; i < (int) sizeof(arr); i++) {
+                bool swap = false;
+                for (int j = 0; j < (int) sizeof(arr); j++) {
+                        if (arr[j] > arr[j + 1]) {
+                                tmp = arr[j];
+                                arr[j] = arr[j + 1];
+                                arr[j + 1] = tmp;
+                                swap = true;
+                        }
+                }
+                if (!swap)
+                        break;
+        }
 }
 
 #endif // __BUBBLESORTS_H_
