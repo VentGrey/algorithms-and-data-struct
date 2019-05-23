ffa71e1a41eb995c100f27847aca55ea0ccfe5ac
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-05-22 22:28:37 -0500

    Added normal bubblesort to the header

---
+++ SortingEnv/bubblesorts.h
@@ -0,0 +1,25 @@
+#ifndef __BUBBLESORTS_H_
+#define __BUBBLESORTS_H_
+
+#include < stdio.h >
+#include < stdbool.h >
+
+
+// -- prototyped
+void bubble(int arr[]);
+
+
+void bubble(int arr[]) {
+        int n = sizeof(arr);
+        for (int i = 0; i < n - 1; i++) {
+                for (int j = 0; j < n - i - 1; j++) {
+                        if (arr[j] > arr[j + 1]) {
+                                int tmp = arr[j];
+                                arr[j] = arr[j + 1];
+                                arr[j + 1] = tmp;
+                        }
+                }
+        }
+}
+
+#endif // __BUBBLESORTS_H_
