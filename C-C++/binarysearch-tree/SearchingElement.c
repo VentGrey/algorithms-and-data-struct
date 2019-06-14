f83000c78f01df7752250b515d2882d3a172beff
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-06-14 18:22:58 -0500

    Corrected code styling to Linux Style

--- C-C++/binarysearch-tree/SearchingElement.c
+++ C-C++/binarysearch-tree/SearchingElement.c
@@ -4,16 +4,16 @@ typedef int Key;
 
 typedef struct _BSTNode
 {
-	Key key;
-	struct _BSTNode * left_child;
-	struct _BSTNode * right_child;
+	    Key key;
+	    struct _BSTNode * left_child;
+	    struct _BSTNode * right_child;
 } BSTNode;
 
 BSTNode* Search(BSTNode* root, Key key)
 {
-	if (root == NULL || root->key == key) return root;
-	else if (root->key > key)
-		return Search(root->left_child, key);
-	else
-		return Search(root->right_child, key);
+	    if (root == NULL || root->key == key) return root;
+	    else if (root->key > key)
+		    return Search(root->left_child, key);
+	    else
+		    return Search(root->right_child, key);
 }
