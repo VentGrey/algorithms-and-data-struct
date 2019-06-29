108828cea787a6cae5fcb6f3ae0073bf0418853d
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-06-29 00:22:30 -0500

    Linux styled code

--- C-C++/DFS_BFS/answer.c
+++ C-C++/DFS_BFS/answer.c
@@ -10,19 +10,17 @@ int queue[MAX];
 //DFS 구현
 int DFS(int v, int N)
 {
- 	int i;
- 	DFSvisit[v] = 1;
- 	printf("%d ", v);
+        int i;
+        DFSvisit[v] = 1;
+        printf("%d ", v);
 
- 	for (i = 1; i <= N; i++)
- 	{
-  		if (Graph[v][i] == 1 && DFSvisit[i] == 0)
-  		{
-   			DFS(i, N);
-  		}
- 	}
- 	return 0;
-	}
+        for (i = 1; i <= N; i++) {
+                if (Graph[v][i] == 1 && DFSvisit[i] == 0) {
+                        DFS(i, N);
+                }
+        }
+        return 0;
+}
 
 
 //BFS구현!!
@@ -30,51 +28,46 @@ int DFS(int v, int N)
 int BFS(int v, int N)
 {
 
- 	int front = 0, rear = 0;
- 	int i, pop;
+        int front = 0, rear = 0;
+        int i, pop;
 
- 	BFSvisit[v] = 1;
- 	printf("%d ", v);
- 	queue[0] = v;
- 	rear++;
+        BFSvisit[v] = 1;
+        printf("%d ", v);
+        queue[0] = v;
+        rear++;
 
- 	while (front < rear)
- 	{
-  		pop = queue[front]; 
-  		front++; 
-  		for (i = 1; i <= N; i++)
-  		{
-   			if (Graph[pop][i] == 1 && BFSvisit[i] == 0)
-   			{
-    				printf("%d ", i);
-    				queue[rear] = i;
-    				rear++;
-    				BFSvisit[i] = 1;
-   			}
-  		}
- 	}	
- 	return 0;
+        while (front < rear) {
+                pop = queue[front];
+                front++;
+                for (i = 1; i <= N; i++) {
+                        if (Graph[pop][i] == 1 && BFSvisit[i] == 0) {
+                                printf("%d ", i);
+                                queue[rear] = i;
+                                rear++;
+                                BFSvisit[i] = 1;
+                        }
+                }
+        }
+        return 0;
 }
 
 
 int main()
 {
- 	int N, M, v;
- 	int x, y;
+        int N, M, v;
+        int x, y;
 
- 	scanf("%d%d%d", &N, &M, &v);
+        scanf("%d%d%d", &N, &M, &v);
 
- 	for (int i = 1; i <= M; i++)
- 	{
-  		scanf("%d%d", &x, &y);
-  		Graph[x][y] = 1;
-  		Graph[y][x] = 1;
+        for (int i = 1; i <= M; i++) {
+                scanf("%d%d", &x, &y);
+                Graph[x][y] = 1;
+                Graph[y][x] = 1;
+        }
 
- 	}
+        DFS(v, N);
+        printf("\n");
+        BFS(v, N);
 
- 	DFS(v, N);
- 	printf("\n");
- 	BFS(v, N);
-
- 	return 0;
+        return 0;
 }
