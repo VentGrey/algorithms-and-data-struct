0515c6240aa3583704549214af1f010b0e0dfbb2
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-06-30 14:54:16 -0500

    More style corrections

--- C-C++/Stack/ParenthesisMatching.c
+++ C-C++/Stack/ParenthesisMatching.c
@@ -4,54 +4,62 @@
 typedef enum {false, true} bool;
 typedef int Data;
 
-typedef struct{
+typedef struct {
     Data items[MAX_STACK];
     int top;
 } Stack;
 
-void InitStack(Stack *pstack){
-    pstack ->top=-1;
-}
-bool IsFull(Stack*pstack){
-    return pstack->top ==MAX_STACK -1;
+void InitStack(Stack *pstack)
+{
+        pstack ->top=-1;
 }
 
-bool IsEmpty(Stack *pstack){
-    return pstack -> top ==-1;
+bool IsFull(Stack*pstack)
+{
+        return pstack->top ==MAX_STACK -1;
 }
 
-Data Peek (Stack *pstack){
-    if (IsEmpty(pstack))
-        exit(1)
-    return pstack->items[pstack->top];
+bool IsEmpty(Stack *pstack)
+{
+        return pstack -> top ==-1;
 }
 
-void Push(Stack*pstack, Data item){
-    if (IsFull(pstack))
-        exit(1);
-    pstack->items[++(pstack->top)]=item;
-}
-void Pop(Stack*pstack){
-    it(IsEmpty(pstack))
-       exit(1);
-    --(pstack->top);
+Data Peek (Stack *pstack)
+{
+        if (IsEmpty(pstack))
+                exit(1)
+        return pstack->items[pstack->top];
 }
 
-bool IsParantBalanced(char*exp, int len){
-    Stack stack;
-    InitStack(&stack);
-    for (int i=0; i<len;i++){
-        if(exp[i]=='(')
-           Push(&stack, exp[i]);
-        else if (exp[i]==')'){
-            if (IsEmpty(&stack))
-               return false;
-            else
-                Pop(&stack);
+void Push(Stack*pstack, Data item)
+{
+        if (IsFull(pstack))
+                exit(1);
+        pstack->items[++(pstack->top)]=item;
+}
+void Pop(Stack*pstack)
+{
+        if(IsEmpty(pstack))
+                exit(1);
+        --(pstack->top);
+}
+
+bool IsParantBalanced(char*exp, int len)
+{
+        Stack stack;
+        InitStack(&stack);
+        for (int i=0; i<len;i++) {
+                if(exp[i]=='(')
+                        Push(&stack, exp[i]);
+                else if (exp[i]==')') {
+                if (IsEmpty(&stack))
+                        return false;
+                else
+                        Pop(&stack);
+                }
         }
-    }
-    if (IsEmpty(&stack))
-       return true;
-    else
-       return false;
+        if (IsEmpty(&stack))
+                return true;
+        else
+                return false;
 }
--- C-C++/Stack/ReversePrint.c
+++ C-C++/Stack/ReversePrint.c
@@ -11,44 +11,53 @@ typedef struct{
     int top;
 } Stack;
 
-void InitStack(Stack *pstack){
-    pstack ->top=-1;
-}
-bool IsFull(Stack*pstack){
-    return pstack->top ==MAX_STACK -1;
+void InitStack(Stack *pstack)
+{
+        pstack ->top=-1;
 }
 
-bool IsEmpty(Stack *pstack){
-    return pstack -> top ==-1;
+bool IsFull(Stack*pstack)
+{
+        return pstack->top ==MAX_STACK -1;
 }
 
-Data Peek (Stack *pstack){
-    if (IsEmpty(pstack))
-        exit(1)
-    return pstack->items[pstack->top];
+bool IsEmpty(Stack *pstack)
+{
+        return pstack -> top == -1;
 }
 
-void Push(Stack*pstack, Data item){
-    if (IsFull(pstack))
-        exit(1);
-    pstack->items[++(pstack->top)]=item;
-}
-void Pop(Stack*pstack){
-    it(IsEmpty(pstack))
-       exit(1);
-    --(pstack->top);
+Data Peek (Stack *pstack)
+{
+        if (IsEmpty(pstack))
+                exit(1);
+        return pstack->items[pstack->top];
 }
 
-void ReversePrint(char*s, int len){
-    Stack stack;
-    char ch;
+void Push(Stack*pstack, Data item)
+{
+        if (IsFull(pstack))
+                exit(1);
+        pstack->items[++(pstack->top)]=item;
+}
 
-    InitStack(&stack);
-    for(int i=0;i<len;i++)
-        Push(&stack, s[i]);
-    while (!IsEmpty(&stack)){
-        ch=Peek(&stack);
-        printf("%c", ch);
-        Pop(&stack);
-    }
+void Pop(Stack*pstack)
+{
+       if(IsEmpty(pstack))
+                exit(1);
+       --(pstack->top);
+}
+
+void ReversePrint(char*s, int len)
+{
+        Stack stack;
+        char ch;
+
+        InitStack(&stack);
+        for(int i=0;i<len;i++)
+                Push(&stack, s[i]);
+        while (!IsEmpty(&stack)) {
+                ch=Peek(&stack);
+                printf("%c", ch);
+                Pop(&stack);
+        }
 }
