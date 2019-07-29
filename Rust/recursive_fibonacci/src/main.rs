29a8e1fecac3eda5435808cbd144a44cf9bcd014
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-07-28 22:49:16 -0500

    Finished

--- Rust/recursive_fibonacci/src/main.rs
+++ Rust/recursive_fibonacci/src/main.rs
@@ -1,3 +1,20 @@
+const FIB_ZERO: u64 = 1;
+const FIB_ONE: u64 = 1;
+
+fn fib(n: u64) -> u64 {
+    if n == 0 {
+        FIB_ZERO
+    } else if n == 1 {
+        FIB_ONE
+    } else {
+        fib(n - 1) + fib(n - 2)
+    }
+}
+
+fn is_zero(n: i64) -> bool { n == 0 }
+
 fn main() {
-    println!("Hello, world!");
+    for i in 1..41 {
+        println!("{}: {}", i, fib(i));
+    }
 }
