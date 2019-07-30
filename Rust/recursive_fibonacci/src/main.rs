0ec0d3dbf8b9aaab835b2a8329ae8fac0e47e5f1
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-07-30 02:16:59 -0500

    Finished func

--- Rust/recursive_fibonacci/src/main.rs
+++ Rust/recursive_fibonacci/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 const FIB_ZERO: u64 = 1;
 const FIB_ONE: u64 = 1;
 
@@ -11,10 +13,29 @@ fn fib(n: u64) -> u64 {
     }
 }
 
-fn is_zero(n: i64) -> bool { n == 0 }
+fn is_zero(n: i64) -> bool {
+    n == 0
+}
+
+fn fib_dyn(n: u64, map: &mut HashMap<u64, u64>) -> u64 {
+    match n {
+        0 | 1 => 1,
+        n => {
+            if map.contains_key(&n) {
+                *map.get(&n).unwrap()
+            } else {
+                let val = fib_dyn(n - 1, map) + fib_dyn(n - 2, map);
+                map.insert(n, val);
+                val
+            }
+        }
+    }
+}
 
 fn main() {
+    let mut map = HashMap::new();
+
     for i in 1..41 {
-        println!("{}: {}", i, fib(i));
+        println!("{}: {}", i, fib_dyn(i, &mut map));
     }
 }
