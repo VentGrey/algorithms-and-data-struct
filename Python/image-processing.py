6e5789e0bb16ca8d4714dd634b372138465d8f88
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-08-28 13:48:23 -0500

    Hacer un cagadero

--- Python/image-processing.py
+++ Python/image-processing.py
@@ -1,1 +1,12 @@
-#!/usr/bin/env python
+import cv2
+
+img = cv2.imread('chrono.png')
+print(img[0][0])
+print(img.shape)
+for row in img:
+    for pixel in row:
+        print(pixel, end=' ')
+
+cv2.imshow('Image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
